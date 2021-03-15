1a8eb5ee41ccf7e4899c20034c4ef4e369f55ea9
unknown <swapnilgodbolecode@gmail.com>
2021-03-15 10:07:36 +0530

    Angular project

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users:any;
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(): void {
+  this.getUsers();
+  }
+  
+  getUsers(){
+    this.users=this.http.get("https://localhost:5001/api/users").subscribe(respose=>{
+      this.users=respose
+    },
+    error=>{
+      console.log(error)
+    })
+  }
+}
